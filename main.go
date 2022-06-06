ec58377ef4df4e9518c0b2198e47adc8753befdc
mattan5271 <mattan.system.engineering@gmail.com>
2022-06-06 15:07:15 +0900

    🗃️ Todo一覧取得ロジック完成

--- main.go
+++ main.go
@@ -2,15 +2,30 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"todo_app/config"
+	"todo_app/app/models"
 )
 
 func main() {
-	fmt.Println(config.Config.Port)
-	fmt.Println(config.Config.SQLDriver)
-	fmt.Println(config.Config.DbName)
-	fmt.Println(config.Config.LogFile)
+	fmt.Println(models.Db)
 
-	log.Println("test")
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.PassWord = "testtest"
+	fmt.Println(u)
+	u.CreateUser()
+
+	user, _ := models.GetUser(2)
+
+	// u.Name = "Test2"
+	// u.Email = "test2@example.com"
+	// u.UpdateUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
+
+	// u.DeleteUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
+
+	user.CreateTodo("First Todo")
 }
