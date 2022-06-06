805180bb3922dfacbebc01213ca041147cd1621b
mattan5271 <mattan.system.engineering@gmail.com>
2022-06-06 14:46:50 +0900

    🗃️ ユーザー削除ロジック完成

--- app/models/users.go
+++ app/models/users.go
@@ -16,20 +16,15 @@ type User struct {
 
 // ユーザー作成
 func (u *User) CreateUser() (err error) {
-	cmd := `insert into users (
-		uuid,
-		name,
-		email,
-		password,
-		created_at) values (?, ?, ?, ?, ?)`
-
-	_, err = Db.Exec(cmd,
+	cmd := `insert into users ( uuid, name, email, password, created_at) values (?, ?, ?, ?, ?)`
+	_, err = Db.Exec(
+		cmd,
 		createUUID(),
 		u.Name,
 		u.Email,
 		Encrypt(u.PassWord),
-		time.Now())
-
+		time.Now(),
+	)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -55,6 +50,16 @@ func GetUser(id int) (user User, err error) {
 func (u *User) UpdateUser() (err error) {
 	cmd := `update users set name = ?, email = ? where id = ?`
 	_, err = Db.Exec(cmd, u.Name, u.Email, u.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
+
+// ユーザー削除
+func (u *User) DeleteUser() (err error) {
+	cmd := `delete from users where id = ?`
+	_, err = Db.Exec(cmd, u.ID)
 	if err != nil {
 		log.Fatalln(err)
 	}
