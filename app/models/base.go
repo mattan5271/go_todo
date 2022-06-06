ceed1f87696198bd2011bb347a03ab6b2ac1d045
mattan5271 <mattan.system.engineering@gmail.com>
2022-06-06 14:57:35 +0900

    🗃️ todosテーブル作成

--- app/models/base.go
+++ app/models/base.go
@@ -17,6 +17,7 @@ var err error
 
 const (
 	tableNameUser = "users"
+	tableNameTodo = "todos"
 )
 
 func init() {
@@ -32,8 +33,14 @@ func init() {
 		email STRING,
 		password STRING,
 		created_at DATETIME)`, tableNameUser)
-
 	Db.Exec(cmdU)
+
+	cmdT := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		content TEXT,
+		user_id INTEGER,
+		created_at DATETIME)`, tableNameTodo)
+	Db.Exec(cmdT)
 }
 
 func createUUID() (uuidobj uuid.UUID) {
